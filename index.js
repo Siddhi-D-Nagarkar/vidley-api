26225838fed1a6242ee363cac5448e2c2e0341d8
Siddhi Nagarkar <siddhinagarkar1@gmail.com>
2020-06-24 23:17:15 +0530

    Connected MongoDb

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 const home = require("./routes/home");
 const app = express();
+
+mongoose.connect("mongodb://localhost/movies",{ useNewUrlParser: true ,useUnifiedTopology: true})
+            .then(() => console.log("Connected To MongoDb"))
+            .catch((err) => console.log(err));
+             
 app.use(express.json());
 app.use("/",home);
 app.use("/api/genres",genres);
+
+
+
 app.listen(3000,() => console.log("Listening on Port 3000 ......"));
--- routes/genres.js
+++ routes/genres.js
@@ -1,76 +1,82 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
+const mongoose = require('mongoose');
 
-var genres =[
-    { id : 1 , value : "Action"},
-    { id : 2 , value : "Adventure"},
-    { id : 3 , value : "Comedy"},
-    { id : 4 , value : "Drama"},
-    { id : 5 , value : "Horro"},
-    { id : 6 , value : "Romance"},
-]
+const Genre = mongoose.model("Genere",new mongoose.Schema({
+    name : {
+        type : String,
+        minlength : 5,
+        maxlength :50,
+        required : true,
+    }
+}));
+// /api/genres
 
-router.get("/",(req , res) => {
+
+
+router.get("/",async (req , res) => {
+    const genres = await Genre.find().sort('name');   
     res.send(genres);
 });
 
-router.get("/:id",(req , res)=>{
+router.get("/:id", async (req , res)=>{
    // const genre = genres.find( (c) =>  c.id === parseInt(req.params.id));
-   const genre = genres.find( c => c.id === parseInt(req.params.id));
+   const genre = await Genre.findById(req.params.id);
+   
    if(!genre) return res.status(404).send("The Course With given id was not found ");
     
     res.send(genre);
 });
 
-router.post("/",(req ,res) => {
+router.post("/",async (req ,res) => {
     const schema = {
-        value : Joi.string().min(3).required()
+        name : Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body , schema);
+    // console.log(result.value);
+    
+    if(result.error){
+        return res.status(400).send(result.error.details[0].message);
+     }
+    // console.log("hi bro");
+    
+    let genre = new Genre({ name : req.body.name });
+    try{
+        genre = await genre.save();
+        console.log(genre);
+    }catch(err){
+        console.log(err);
+    }
+    res.send(genre);
+
+});
+
+router.put("/:id",async (req , res) => {
+    const schema = {
+        name : Joi.string().min(3).required()
     }
 
     const result = Joi.validate(req.body , schema);
 
     if(result.error){
         return res.status(400).send(result.error.details[0].message);
-        
     }
 
-    const genre = {
-        id : genres.length+1,
-        value : req.body.value,
-    }
-    genres.push(genre);
-    res.send(genre);
+    const genre = await Genre.findByIdAndUpdate(req.params.id , { name : req.body.name},{ new : true , useFindAndModify: false});
 
-});
+    if(!genre) return res.status(404).send("The Course With given id was not found ");
 
-router.put("/:id",(req , res) => {
-    const genre = genres.find( c => c.id === parseInt(req.params.id));
-   if(!genre) return res.status(404).send("The Course With given id was not found ");
-
-    const schema = {
-        value : Joi.string().min(3).required()
-    }
-
-    const result = Joi.validate(req.body , schema);
-
-    if(result.error){
-        return res.status(400).send(result.error.details[0].message);
-        
-    }
-
-    genre.value=req.body.value;
     res.send(genre);
 });
 
 
-router.delete("/:id",(req, res) => {
-    const genre = genres.find( c => c.id === parseInt(req.params.id));
+router.delete("/:id",async (req, res) => {
+
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+
    if(!genre) return res.status(404).send("The Course With given id was not found ");
-
-    const index = genres.indexOf(genre);
-    genres.splice(index,1);
-
     res.send(genre);
 });
 
