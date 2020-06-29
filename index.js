4e4f3e09aabb65e86dba45e35ff4298242c557db
Siddhi Nagarkar <siddhinagarkar1@gmail.com>
2020-06-30 01:21:10 +0530

    Added Logging Error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
-
+const winston = require('winston');
+require('winston-mongodb')
 Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');   
 const mongoose = require('mongoose');
@@ -11,7 +12,34 @@ const movies = require('./routes/movies');
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require('./middleware/error');
 const app = express();
+
+//To Handle errors without the context of express example line no 32
+process.on('uncaughtException',(err) => {
+    console.log(" We caught an uncaught exception");
+    winston.error(err.message,err);
+});
+
+process.on('unhandledRejection',(err) => {
+    console.log(" We caught an uncaught rejection");
+    winston.error(err.message,err);
+});
+
+
+winston.configure({
+    transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: 'logfile.log' }),
+    ]
+  });
+  winston.add(new winston.transports.MongoDB({db :'mongodb://localhost/vidley'}));
+
+//throw new Error("Uncaught exception")
+const p = Promise.reject(new Error("Uncaught Promise"))
+.then(() => log.error(err.message));
+
+
 
 if(!config.get("jwtPrivateKey")){
     console.error("FATAL ERROR : jwtPrivateKey is not defined");
@@ -19,10 +47,13 @@ if(!config.get("jwtPrivateKey")){
 }
 
 
+
+
 mongoose.connect("mongodb://localhost/vidley",{ useNewUrlParser: true ,useUnifiedTopology: true})
             .then(() => console.log("Connected To MongoDb"))
             .catch((err) => console.log(err));
              
+
 app.use(express.json());
 app.use("/",home);
 app.use("/api/genres",genres);
@@ -32,6 +63,6 @@ app.use("/api/rentals",rentals);
 app.use("/api/users",users);
 app.use("/api/auth",auth);
 
-
+app.use(error);
 
 app.listen(3000,() => console.log("Listening on Port 3000 ......"));
--- routes/genres.js
+++ routes/genres.js
@@ -5,34 +5,33 @@ const Joi = require('joi');
 const { User } = require('../models/user');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const asyncMiddleware = require('../middleware/async')
 // /api/genres
 
-router.get("/",async (req , res) => {
-    const genres = await Genre.find().sort('name');   
-    res.send(genres);
-});
+router.get("/",asyncMiddleware(async (req , res , next) => {
+        throw new Error("Could not get genres")
+        const genres = await Genre.find().sort('name');   
+        res.send(genres);
+}));
 
-router.get("/:id", async (req , res)=>{
+router.get("/:id",asyncMiddleware (async (req , res)=>{
    // const genre = genres.find( (c) =>  c.id === parseInt(req.params.id));
    const genre = await Genre.findById(req.params.id);
    
    if(!genre) return res.status(404).send("The Course With given id was not found ");
     
     res.send(genre);
-});
+}));
 
-router.post("/",auth,async (req ,res) => {
+router.post("/",auth,asyncMiddleware( async (req ,res) => {
 const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
-
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
-  
   res.send(genre);
+}));
 
-});
-
-router.put("/:id",async (req , res) => {
+router.put("/:id",asyncMiddleware( async (req , res) => {
     const result = Joi.validate(req.body);
 
     if(result.error){
@@ -44,15 +43,15 @@ router.put("/:id",async (req , res) => {
     if(!genre) return res.status(404).send("The Course With given id was not found ");
 
     res.send(genre);
-});
+}));
 
 
-router.delete("/:id",[auth,admin],async (req, res) => {
+router.delete("/:id",[auth,admin],asyncMiddleware( async (req, res) => {
 
     const genre = await Genre.findByIdAndRemove(req.params.id);
 
    if(!genre) return res.status(404).send("The Course With given id was not found ");
     res.send(genre);
-});
+}));
 
 module.exports = router;
